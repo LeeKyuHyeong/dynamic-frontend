ac0f1da7d967265b7ce452077fe3e8d3a1e5e56f
이규형 <rbgud2380@naver.com>
2023-02-05 23:46:18 +0900

    after Feb i will do this with react

---
+++ react_css/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Line from "./html/2023_02/230201_line";
+import { BrowserRouter, Routes, Link, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+
+      <Line />
+    </div>
+  );
+}
+
+export default App;
---
+++ react_css/src/html/2023_02/230201_line.js
@@ -0,0 +1,12 @@
+import { React } from 'react';
+import "../../css/2023_02/230201_line.css";
+
+const Line = () => {
+  return (
+    <div className='container'>
+      <div className="loader"></div>
+    </div>
+  )
+}
+
+export default Line;
