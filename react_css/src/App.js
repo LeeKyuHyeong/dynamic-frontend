ae47a89929b81692ce623b44c4a070cc02951457
이규형 <rbgud2380@naver.com>
2023-02-08 21:20:28 +0900

    230203 mini calendar finished

--- react_css/src/App.js
+++ react_css/src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import Line from "./html/2023_02/230201_line";
-import Glow from './html/2023_02/230202_3D_glowing';
 import Calendar from './html/2023_02/230203_mini_calendar';
 //import { BrowserRouter, Routes, Link, Route } from "react-router-dom";
 
--- react_css/src/html/2023_02/230203_mini_calendar.js
+++ react_css/src/html/2023_02/230203_mini_calendar.js
@@ -2,9 +2,42 @@ import React from "react";
 import "../../css/2023_02/230203_mini_calendar.css";
 
 const Calendar = () => {
+
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+  let day = new Date();
+  let dayNum = day.getDay();
+  let dayList = document.querySelectorAll('.week li');
+
+  let h1 = document.createElement('h1');
+  h1.innerHTML = day.getDate();
+
+  let h3 = document.createElement('h3');
+  h3.innerHTML = day.getFullYear();
+
+  let h5 = document.createElement('h5');
+  h5.innerHTML = months[day.getMonth()];
+
+  dayList.forEach((e, i) => {
+    if (i === dayNum - 1) {
+      e.classList.add('current');
+      e.appendChild(h1);
+      e.appendChild(h3);
+      e.appendChild(h5);
+    }
+  })
+
   return (
     <div className="wrap">
-
+      <ul className="week">
+        <li>Mon</li>
+        <li>Tue</li>
+        <li>Wed</li>
+        <li>Thu</li>
+        <li>Fri</li>
+        <li>Sat</li>
+        <li>Sun</li>
+      </ul>
     </div>
   )
 }
