b6f7a47b1058ce92bcd0de688aa95fc6ff15f7ef
이규형 <rbgud2380@naver.com>
2023-02-11 12:22:54 +0900

    230211 useNavigate

--- react_css/src/App.js
+++ react_css/src/App.js
@@ -3,11 +3,16 @@ import Calendar from './html/2023_02/230203_mini_calendar';
 import Glow from './html/2023_02/230202_3D_glowing';
 import Line from './html/2023_02/230201_line';
 import Clock from './html/2023_02/230204_analog_clock';
-import { Routes, Link, Route } from "react-router-dom";
+import { Routes, Link, Route, useNavigate } from "react-router-dom";
 
 function App() {
+  const history = useNavigate();
+  const movetoMain = () => {
+    history("/");
+  }
   return (
     <div className="App">
+      <button onClick={movetoMain}>Main</button>
       <Link to="/230201Line">Line</Link>
       <Link to="/230202Glow">Glow</Link>
       <Link to="/230203Calendar">Calendar</Link>
